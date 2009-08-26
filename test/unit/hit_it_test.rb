e166bddd14b26b7c00e7bd5d800d799954457618
Jordan McKible <jordan@mckible.com>
2009-08-25 23:47:15 -0700

    Declarative test syntax.

--- test/unit/hit_it_test.rb
+++ test/unit/hit_it_test.rb
@@ -2,21 +2,21 @@ require 'test_helper'
 
 class HitItTest < ActiveSupport::TestCase
   
-  def test_has_many_hits
+  test 'has many hits' do
     assert_equal hits(:home_january, :home_february), pages(:home).hits
   end
   
-  def test_sum_hits
+  test 'sum hits' do
     assert_equal 14, pages(:home).hits.sum(:count)
     assert_equal 10, pages(:home).hits.for(2009, 1).sum(:count)
   end
   
-  def test_hit_existing_month
+  test 'hit existing month' do
     pages(:home).hit!(Date.new(2009, 1, 1))
     assert_equal 11, hits(:home_january).count
   end
   
-  def test_hit_new_month
+  test 'hit new month' do
     hit_count = Hit.count
     pages(:home).hit!(Date.new(2009, 3, 1))
     assert_equal (hit_count+1), Hit.count
--- test/unit/hit_test.rb
+++ test/unit/hit_test.rb
@@ -2,29 +2,29 @@ require 'test_helper'
 
 class HitTest < ActiveSupport::TestCase
   
-  def test_belongs_to_hitable
+  test 'belongs to hitable' do
     assert_equal pages(:home), hits(:home_january).hitable
   end
   
-  def test_requires_hitable
+  test 'requires hitable' do
     hit = Hit.create
     assert_match /blank/, hit.errors[:hitable_id].to_s
     assert_match /blank/, hit.errors[:hitable_type].to_s
   end
   
-  def test_requires_date
+  test 'requires date' do
     hit = Hit.create
     assert_match /blank/, hit.errors[:year].to_s
     assert_match /blank/, hit.errors[:month].to_s
   end
   
-  def test_unique_relationship
+  test 'unique relationship' do
     hit = hits(:home_january).clone
     hit.valid?
     assert_match /taken/, hit.errors[:month].to_s
   end
   
-  def test_for_named_scope
+  test 'for named scoped' do
     assert_equal [hits(:home_january)], Hit.for(2009, 1)
   end
   
